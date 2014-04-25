8172ac458f6203725ee25051d4bd33aa6575a511
iVoid <xin.song.free@gmail.com>
2014-04-25 15:03:40 +0800

    modify style

--- assets/themes/dinky/js/scale.fix.js
+++ assets/themes/dinky/js/scale.fix.js
@@ -18,3 +18,12 @@ fixScale = function(doc) {
 	}
 
 };
+WB2.anyWhere(function(W){
+    W.widget.followButton({
+        'nick_name': '宋鑫Keep_Learning',  //用户昵称
+        'id': "wb_follow_btn",
+        'show_head' : true, //是否显示头像
+        'show_name' : true, //是否显示名称
+        'show_cancel': true //是否显示取消关注按钮
+    });
+});
